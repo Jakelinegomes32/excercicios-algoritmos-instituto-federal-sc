5ff0d36c69e017ac483adfc83795f00deb46bed6
Jakeline Gomes <jakeline5793@gmail.com>
2025-08-25 19:57:01 -0300

    Update exc-02.js

--- exc-02.js
+++ exc-02.js
@@ -1,5 +1,5 @@
 var salario, porcentagem, novo_salario;
 salario = prompt ("enter seu salario");
 porcentagem = prompt ("enter porcentagem");
-novo-salario = salario * (1 + porcentagem / 100)
+novo_salario = salario * (1 + porcentagem / 100)
 console.log("novo salario:", novo_salario);
