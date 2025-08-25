183cfa25536358dda4769d6eefa723dad821e712
Aluno <jakeline5793@gmail.com>
2025-08-25 15:24:56 -0300

    exc-01.js

---
+++ exc-01.js
@@ -0,0 +1,3 @@
+let salarioAtual = parseFloat(prompt("Digite o salário atual do funcionário:"));
+let novoSalario = salarioAtual * 1.25;
+alert("O novo salário com 25% de aumento é: R$ " + novoSalario.toFixed(2));
