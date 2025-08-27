b38d582abc3c7d334f7e0daed5302c4395778268
Jakeline Gomes <jakeline5793@gmail.com>
2025-08-26 22:11:33 -0300

    Delete exc-02.js

--- exc-02.js
+++
@@ -1,5 +0,0 @@
-var salario, porcentagem, novo_salario;
-salario = prompt ("enter seu salario");
-porcentagem = prompt ("enter porcentagem");
-novo_salario = salario * (1 + porcentagem / 100)
-console.log("novo salario:", novo_salario);
