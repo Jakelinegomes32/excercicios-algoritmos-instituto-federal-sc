28c5bd2a33ecbeb6a05e6b0b055ce6d31847c3b8
jakeline <jakeline5793@gmail.com>
2025-08-28 12:18:31 -0300

    exc-12.js

---
+++ exc-12.js
@@ -0,0 +1,6 @@
+let numero = parseInt(prompt("Digite um número inteiro:"));
+let antecessor = numero - 1;
+let sucessor = numero + 1;
+console.log("Número digitado: " + numero);
+console.log("Antecessor: " + antecessor);
+console.log("Sucessor: " + sucessor);
