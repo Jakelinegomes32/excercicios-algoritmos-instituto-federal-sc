3655b629fb27f33f38884b2a2d6a2f8bdfc6db06
jakeline <jakeline5793@gmail.com>
2025-08-28 11:18:11 -0300

    exc-06.js

---
+++ exc-06.js
@@ -0,0 +1,5 @@
+let comprimento = parseFloat(prompt("Digite o comprimento da caixa:"));
+let largura = parseFloat(prompt("Digite a largura da caixa:"));
+let altura = parseFloat(prompt("Digite a altura da caixa:"));
+let volume = comprimento * largura * altura;
+console.log("O volume da caixa retangular é: " + volume);
