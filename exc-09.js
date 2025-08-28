a3348e08910fb0e8bcd36f0fa22d585164d01993
jakeline <jakeline5793@gmail.com>
2025-08-28 12:04:17 -0300

    exc-09.js

---
+++ exc-09.js
@@ -0,0 +1,6 @@
+let A = parseInt(prompt("Digite o valor de A (inteiro):"));
+let B = parseInt(prompt("Digite o valor de B (inteiro):"));
+let C = parseInt(prompt("Digite o valor de C (inteiro):"));
+let soma = A + B + C;
+let resultado = soma ** 2;
+console.log("O quadrado da soma de A + B + C é: " + resultado);
