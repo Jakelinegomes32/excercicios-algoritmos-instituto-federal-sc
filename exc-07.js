74298f5b170e0970cce8efcf4a51fe516e15e2b1
jakeline <jakeline5793@gmail.com>
2025-08-28 11:31:11 -0300

    exc-07.js

---
+++ exc-07.js
@@ -0,0 +1,4 @@
+let A = parseInt(prompt("Digite o valor de A (inteiro):"));
+let B = parseInt(prompt("Digite o valor de B (inteiro):"));
+let resultado = (A-B) ** 2;
+console.log("o quadrado da diferença entre A e B é:"+ resultado);
